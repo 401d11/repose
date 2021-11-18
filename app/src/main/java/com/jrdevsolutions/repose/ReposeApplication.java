4fc7748589c7e6af0b426595dcad6eef3ffb0a6f
MattyCho <myc.chosen@gmail.com>
2021-11-18 12:11:00 -0800

    initial android setup, amplify setup

---
+++ app/src/main/java/com/jrdevsolutions/repose/ReposeApplication.java
@@ -0,0 +1,24 @@
+package com.jrdevsolutions.repose;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.core.Amplify;
+
+public class ReposeApplication extends Application {
+
+    public final static String TAG = "jrdevsolutions_repose_application";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.configure(getApplicationContext());
+        } catch (AmplifyException ae) {
+            Log.e(TAG, "Error initializing Amplify: " + ae.getMessage(), ae);
+        }
+    }
+}
