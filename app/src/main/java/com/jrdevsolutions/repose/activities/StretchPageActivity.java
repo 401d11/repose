8af09e0bd43449269b8651894602f2f359670502
hkimbrough22 <haustin.kimbrough@hotmail.com>
2021-11-19 15:18:06 -0500

    fine tunes countdown Timer

--- app/src/main/java/com/jrdevsolutions/repose/activities/StretchPageActivity.java
+++ app/src/main/java/com/jrdevsolutions/repose/activities/StretchPageActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,11 +17,24 @@ import java.util.Timer;
 public class StretchPageActivity extends AppCompatActivity {
 
         TextView timerTextView;
-        long startTime = 0;
-        boolean isTimerAlreadyRunning = false;
+        Button timerButton;
+        long millisLeft = 30000;
+        long min = 0;
+        long sec = 0;
 
+    boolean isTimerAlreadyRunning;
+        CountDownTimer cdTimer;
 
-        //runs without a timer by reposting this handler at the end of the runnable
+    public void timerPause() {
+        cdTimer.cancel();
+    }
+
+    private void timerResume() {
+        Log.i("min", Long.toString(min));
+        Log.i("Sec", Long.toString(sec));
+        timerHandler.postDelayed(timerRunnable, 0);
+    }
+
         Handler timerHandler = new Handler();
         Runnable timerRunnable = new Runnable() {
 
@@ -28,29 +42,31 @@ public class StretchPageActivity extends AppCompatActivity {
             @SuppressLint("DefaultLocale")
             @Override
             public void run() {
-                new CountDownTimer(30000, 1000) {
-                    public void onTick(long millisUntilFinished) {
-                        if(millisUntilFinished >= 10000){
-                        timerTextView.setText(String.format("0:%d", millisUntilFinished / 1000));
-                        } else timerTextView.setText(String.format("0:0%d", millisUntilFinished / 1000));
+                cdTimer = new CountDownTimer(millisLeft, 1000) {
+                    @SuppressLint("SetTextI18n")
+                    public void onTick(long milliTillFinish) {
 
+                        millisLeft = milliTillFinish;
+                        min = (milliTillFinish / (1000 * 60));
+                        sec = ((milliTillFinish / 1000) - min * 60);
 
-//                                "Seconds remaining: " + millisUntilFinished / 1000);
+                        if(sec >= 10){
+                        timerTextView.setText(Long.toString(min) + ":" + Long.toString(sec));
+                        } else timerTextView.setText(Long.toString(min) + ":0" + Long.toString(sec));
+
+                        Log.i("Tick", "Tock");
                     }
+
 
                     public void onFinish() {
                         timerTextView.setText("Done");
+                        millisLeft = 30000;
+                        timerButton.setText("Start");
                         isTimerAlreadyRunning = false;
                     }
-                }.start();
-//                long millis = System.currentTimeMillis() - startTime;
-//                int seconds = (int) (millis / 1000);
-//                int minutes = seconds / 60;
-//                seconds = seconds % 60;
+                };
 
-//                timerTextView.setText(String.format("%d:%02d", minutes, seconds));
-
-//                timerHandler.postDelayed(this, 500);
+                cdTimer.start();
             }
         };
 
@@ -59,18 +75,25 @@ public class StretchPageActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_stretch_page);
 
+            isTimerAlreadyRunning = false;
+
             timerTextView = (TextView) findViewById(R.id.timerTextView);
 
-            Button timerButton = (Button) findViewById(R.id.timerButton);
+            timerButton = (Button) findViewById(R.id.timerButton);
             timerButton.setText(R.string.Start);
             timerButton.setOnClickListener(view -> {
-                if (timerButton.getText().equals("Stop")) {
-                    timerHandler.removeCallbacks(timerRunnable);
-                    timerButton.setText(R.string.Start);
-                } else if(timerButton.getText().equals("Start") && isTimerAlreadyRunning){
-                    startTime = System.currentTimeMillis();
+                if(timerButton.getText().equals("Start")){
+                    Log.i("Started", timerButton.getText().toString());
+                    timerButton.setText("Pause");
                     timerHandler.postDelayed(timerRunnable, 0);
-                    timerButton.setText(R.string.Stop);
+//                    cdTimer.onTick(30*1000);
+                } else if (timerButton.getText().equals("Pause")){
+                    Log.i("Paused", timerButton.getText().toString());
+                    timerButton.setText("Resume");
+                    timerPause();
+                } else if (timerButton.getText().equals("Resume")){
+                    timerButton.setText("Pause");
+                    timerResume();
                 }
             });
         }
@@ -82,5 +105,7 @@ public class StretchPageActivity extends AppCompatActivity {
             Button b = (Button)findViewById(R.id.timerButton);
             b.setText(R.string.Start);
         }
+
+
 
 }
